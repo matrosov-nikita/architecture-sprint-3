84531bac74f92ea8980ce3c0ea33164d78458e77
Nikita Matrosov <matrosov.nikita94@gmail.com>
2024-12-08 03:20:31 +0300

    progress

--- microservices/smart-telemetry-service/internal/usecases/dto/dto.go
+++ microservices/smart-telemetry-service/internal/usecases/dto/dto.go
@@ -3,7 +3,7 @@ package dto
 import "time"
 
 type SensorTemperatureEvent struct {
-	DeviceId    int       `json:"deviceId"`
+	DeviceId    int       `json:"device_id"`
 	Temperature float64   `json:"temperature"`
 	Type        string    `json:"type"`
 	OccuredOn   time.Time `json:"occured_on"`
